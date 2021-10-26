61bc19ac8bb5b3a9dd724389d9a33c98169a34d9
Surya Kanigolla <suryakanigolla@gmail.com>
2021-10-26 13:27:09 +0530

    Added timestamps to Comment Schema

--- modals/Comment.js
+++ modals/Comment.js
@@ -1,27 +1,32 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const CommentSchema = new Schema({
-  comment: {
-    type: String,
-    required: true,
-  },
-  user: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
-  likes: [
-    {
+const CommentSchema = new Schema(
+  {
+    comment: {
+      type: String,
+      required: true,
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    likes: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    replyTo: {
       type: Schema.Types.ObjectId,
       ref: "User",
     },
-  ],
-  replyTo: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
   },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const Comment = mongoose.model("Comment", CommentSchema);
 module.exports = Comment;
