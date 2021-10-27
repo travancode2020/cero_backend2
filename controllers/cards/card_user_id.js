250962f2490e68f18bf1ad7c318e9a47e58bb7b7
Surya Kanigolla <suryakanigolla@gmail.com>
2021-10-27 20:21:44 +0530

    Added host details in get cards by user id

--- controllers/cards/card_user_id.js
+++ controllers/cards/card_user_id.js
@@ -4,12 +4,21 @@ const getCardsByUserId = async (req, res, next) => {
   try {
     const userId = req.params.userId;
 
-    const foundCards = await Cards.find({ host: userId });
+    const populateQuery = [
+      {
+        path: "host",
+        select: ["name", "userName", "photoUrl", "fId", "userTag"],
+      },
+    ];
+
+    const foundCards = await Cards.find({ host: userId }).populate(
+      populateQuery
+    );
 
     if (!foundCards) {
       return res.status(404).json({ message: "Cards not found" });
     }
-    
+
     res.status(200).json(foundCards);
   } catch (error) {
     next(error);
