2eb03d623ec3ca9a4c5e9c9629f7e52e983c67d7
Surya Kanigolla <suryakanigolla@gmail.com>
2021-11-01 11:28:07 +0530

    Refactored Location Search and Added Documentation

--- controllers/location/search.js
+++ controllers/location/search.js
@@ -4,9 +4,9 @@ const Users = require("../../modals/User.js");
 const locationBasedSearch = async (req, res, next) => {
   try {
     const range = req.query.range;
-    const tag = req.query.tag;
     const lat = req.query.lat;
     const lng = req.query.lng;
+    const tag = req.query.tag;
     const pro = req.query.pro;
 
     if (!lat || !lng) {
@@ -19,13 +19,13 @@ const locationBasedSearch = async (req, res, next) => {
       return res.status(400).json({ message: "Range is required" });
     }
 
-    if (pro === undefined || pro === null) {
-      return res.status(400).json({ message: "isPro flag is missing" });
-    }
-
     await Location.ensureIndexes({ point: "2dsphere" });
 
-    if (tag) {
+    const populatePath = "host";
+    const populateSelect =
+      "-following -saved -liked -viewed -isLocationSharingEnabled -fId -email -password -dob -work  -location";
+
+    if (tag && pro) {
       await Location.find({
         location: {
           $near: {
@@ -34,11 +34,41 @@ const locationBasedSearch = async (req, res, next) => {
           },
         },
       })
-        .populate(
-          "host",
-          "-following -saved -liked -viewed -isLocationSharingEnabled -fId -email -password -dob -work  -location",
-          { userTag: tag, isPro: pro }
-        )
+        .populate(populatePath, populateSelect, { userTag: tag, isPro: pro })
+        .exec((err, users) => {
+          if (err) {
+            next(err);
+          }
+          users = users.filter((user) => user.host);
+          res.status(200).json(users);
+        });
+    } else if (tag && !pro) {
+      await Location.find({
+        location: {
+          $near: {
+            $geometry: { type: "Point", coordinates: [lng, lat] },
+            $maxDistance: range,
+          },
+        },
+      })
+        .populate(populatePath, populateSelect, { userTag: tag })
+        .exec((err, users) => {
+          if (err) {
+            next(err);
+          }
+          users = users.filter((user) => user.host);
+          res.status(200).json(users);
+        });
+    } else if (!tag && pro) {
+      await Location.find({
+        location: {
+          $near: {
+            $geometry: { type: "Point", coordinates: [lng, lat] },
+            $maxDistance: range,
+          },
+        },
+      })
+        .populate(populatePath, populateSelect, { isPro: pro })
         .exec((err, users) => {
           if (err) {
             next(err);
@@ -55,7 +85,7 @@ const locationBasedSearch = async (req, res, next) => {
           },
         },
       })
-        .populate("host", null, { isPro: pro })
+        .populate(populatePath, populateSelect)
         .exec((err, users) => {
           if (err) {
             next(err);
@@ -68,6 +98,70 @@ const locationBasedSearch = async (req, res, next) => {
     next(error);
   }
 };
+
+// const locationBasedSearch = async (req, res, next) => {
+//   try {
+//     const range = req.query.range;
+//     const tag = req.query.tag;
+//     const lat = req.query.lat;
+//     const lng = req.query.lng;
+//     const pro = req.query.pro;
+
+//     if (!lat || !lng) {
+//       return res
+//         .status(400)
+//         .json({ message: "Coordinates are required for search" });
+//     }
+
+//     if (!range) {
+//       return res.status(400).json({ message: "Range is required" });
+//     }
+
+//     await Location.ensureIndexes({ point: "2dsphere" });
+
+//     if (tag && pro) {
+//       await Location.find({
+//         location: {
+//           $near: {
+//             $geometry: { type: "Point", coordinates: [lng, lat] },
+//             $maxDistance: range,
+//           },
+//         },
+//       })
+//         .populate(
+//           "host",
+//           "-following -saved -liked -viewed -isLocationSharingEnabled -fId -email -password -dob -work  -location",
+//           { userTag: tag, isPro: pro }
+//         )
+//         .exec((err, users) => {
+//           if (err) {
+//             next(err);
+//           }
+//           users = users.filter((user) => user.host);
+//           res.status(200).json(users);
+//         });
+//     } else {
+//       await Location.find({
+//         location: {
+//           $near: {
+//             $geometry: { type: "Point", coordinates: [lng, lat] },
+//             $maxDistance: range,
+//           },
+//         },
+//       })
+//         .populate("host", null, { isPro: pro })
+//         .exec((err, users) => {
+//           if (err) {
+//             next(err);
+//           }
+//           users = users.filter((user) => user.host);
+//           res.status(200).json(users);
+//         });
+//     }
+//   } catch (error) {
+//     next(error);
+//   }
+// };
 
 module.exports = {
   locationBasedSearch,
