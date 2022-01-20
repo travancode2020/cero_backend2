a7f69e35d444f17c54965812a28d1c32337af83c
Jenish <kavathiyajenish008@gmail.com>
2022-01-20 18:57:01 +0530

    add agora route

--- routes/AgoraRouter.js
+++ routes/AgoraRouter.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const {
   generateAgoraToken,
+  generateRtmAgoraToken,
 } = require("../controllers/agoraToken/generateAgoraToken");
 
 const AgoraRouter = express.Router();
 
 AgoraRouter.get("/getToken", generateAgoraToken);
+AgoraRouter.get("/getRtmToken", generateRtmAgoraToken);
 
 module.exports = AgoraRouter;
