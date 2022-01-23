2f6c6156f2b0f088072b7b0ea0dd26a324bf191d
Jenish <kavathiyajenish008@gmail.com>
2022-01-24 00:49:58 +0530

    add hostData

--- controllers/rooms/main.js
+++ controllers/rooms/main.js
@@ -1,4 +1,6 @@
 const Rooms = require("../../modals/Room");
+const Users = require("../../modals/User.js");
+
 const { Types } = require("mongoose");
 const moment = require("moment");
 const addRooms = async (req, res, next) => {
@@ -142,15 +144,23 @@ const getRoomByHost = async (req, res, next) => {
     if (!hostId) throw new Error("Please pass host id");
 
     let rooms = await Rooms.find({ hostId })
+      .select(
+        "specialGuest inviteOrScheduledUser name dateAndTime isPrivate createdAt updatedAt "
+      )
       .sort({ createdAt: -1 })
       .skip(skip)
       .limit(limit);
     if (!rooms) throw new Error("Rooms not found");
+    let hostData = await Users.findOne({ _id: hostId }).select(
+      "_id userName name photoUrl"
+    );
+
+    let data = { rooms: rooms, hostData };
 
     let count = await Rooms.find({ hostId });
     let totalPages = Math.ceil(count.length / limit);
 
-    rooms && res.status(200).json({ succes: true, totalPages, data: rooms });
+    rooms && res.status(200).json({ succes: true, totalPages, data: data });
   } catch (error) {
     next(error);
   }
@@ -198,100 +208,197 @@ const UpdateRoom = async (req, res, next) => {
 };
 
 const getLiveRooms = async (req, res, next) => {
-  let { id, page, limit } = req.query;
-  page = page ? Number(page) : 1;
-  limit = limit ? Number(limit) : 20;
-  let skip = (page - 1) * limit;
-
-  let timeNow = new Date(moment().format("YYYY-MM-DD HH:MM"));
-  let afterOneHour = new Date(
-    moment().add(1, "hours").format("YYYY-MM-DD HH:MM")
-  );
-  let rooms = await Rooms.find({
-    $and: [
-      { dateAndTime: { $gte: timeNow } },
-      { dateAndTime: { $lte: afterOneHour } },
-      {
-        $or: [
-          { isPrivate: false },
-          {
-            $and: [{ isPrivate: true }, { inviteOrScheduledUser: { $in: id } }],
-          },
-        ],
-      },
-    ],
-  });
-  let roomsdata = rooms.slice(skip, skip + limit);
-  let totalPages = Math.ceil(rooms.length / limit);
-  rooms && res.status(200).json({ totalPages, data: roomsdata });
   try {
+    let { id, page, limit } = req.query;
+    page = page ? Number(page) : 1;
+    limit = limit ? Number(limit) : 20;
+    let skip = (page - 1) * limit;
+
+    let timeNow = new Date(moment().format("YYYY-MM-DD HH:MM"));
+    let afterOneHour = new Date(
+      moment().add(1, "hours").format("YYYY-MM-DD HH:MM")
+    );
+    let filter = {
+      $and: [
+        { dateAndTime: { $gte: timeNow } },
+        { dateAndTime: { $lte: afterOneHour } },
+        {
+          $or: [
+            { isPrivate: false },
+            {
+              $and: [
+                { isPrivate: true },
+                { inviteOrScheduledUser: { $in: [Types.ObjectId(id)] } },
+              ],
+            },
+          ],
+        },
+      ],
+    };
+    let rooms = await Rooms.aggregate([
+      { $match: filter },
+      {
+        $lookup: {
+          from: "users",
+          localField: "hostId",
+          foreignField: "_id",
+          as: "hostData",
+        },
+      },
+      { $unwind: "$hostData" },
+      {
+        $project: {
+          "hostData._id": 1,
+          "hostData.userName": 1,
+          "hostData.name": 1,
+          "hostData.photoUrl": 1,
+          specialGuest: 1,
+          inviteOrScheduledUser: 1,
+          name: 1,
+          dateAndTime: 1,
+          isPrivate: 1,
+          createdAt: 1,
+          updatedAt: 1,
+        },
+      },
+    ]);
+
+    let roomsdata = rooms.slice(skip, skip + limit);
+    let totalPages = Math.ceil(rooms.length / limit);
+    rooms && res.status(200).json({ totalPages, data: roomsdata });
   } catch (error) {
     next(error);
   }
 };
 
 const getScheduledRoom = async (req, res, next) => {
-  let { id, page, limit } = req.query;
-  page = page ? Number(page) : 1;
-  limit = limit ? Number(limit) : 20;
-  let skip = (page - 1) * limit;
-
-  let timeNow = new Date(moment().format("YYYY-MM-DD HH:MM"));
-  let afterOneHour = new Date(
-    moment().add(1, "hours").format("YYYY-MM-DD HH:MM")
-  );
-  let rooms = await Rooms.find({
-    $and: [
-      { dateAndTime: { $gte: afterOneHour } },
-      {
-        $or: [
-          {
-            $and: [{ isPrivate: true }, { inviteOrScheduledUser: { $in: id } }],
-          },
-        ],
-      },
-    ],
-  });
-  let roomsdata = rooms.slice(skip, skip + limit);
-  let totalPages = Math.ceil(rooms.length / limit);
-  rooms && res.status(200).json({ totalPages, data: roomsdata });
   try {
+    let { id, page, limit } = req.query;
+    page = page ? Number(page) : 1;
+    limit = limit ? Number(limit) : 20;
+    let skip = (page - 1) * limit;
+
+    let timeNow = new Date(moment().format("YYYY-MM-DD HH:MM"));
+    let afterOneHour = new Date(
+      moment().add(1, "hours").format("YYYY-MM-DD HH:MM")
+    );
+    let filter = {
+      $and: [
+        { dateAndTime: { $gte: afterOneHour } },
+        {
+          $or: [
+            {
+              isPrivate: false,
+              inviteOrScheduledUser: { $in: [Types.ObjectId(id)] },
+            },
+
+            {
+              isPrivate: true,
+              inviteOrScheduledUser: { $in: [Types.ObjectId(id)] },
+            },
+          ],
+        },
+      ],
+    };
+    let rooms = await Rooms.aggregate([
+      { $match: filter },
+      {
+        $lookup: {
+          from: "users",
+          localField: "hostId",
+          foreignField: "_id",
+          as: "hostData",
+        },
+      },
+      { $unwind: "$hostData" },
+      {
+        $project: {
+          "hostData._id": 1,
+          "hostData.userName": 1,
+          "hostData.name": 1,
+          "hostData.photoUrl": 1,
+          specialGuest: 1,
+          inviteOrScheduledUser: 1,
+          name: 1,
+          dateAndTime: 1,
+          isPrivate: 1,
+          createdAt: 1,
+          updatedAt: 1,
+        },
+      },
+    ]);
+
+    let roomsdata = rooms.slice(skip, skip + limit);
+    let totalPages = Math.ceil(rooms.length / limit);
+    rooms && res.status(200).json({ totalPages, data: roomsdata });
   } catch (error) {
     next(error);
   }
 };
 
 const getUpcommingRoom = async (req, res, next) => {
-  let { id, page, limit } = req.query;
-  page = page ? Number(page) : 1;
-  limit = limit ? Number(limit) : 20;
-  let skip = (page - 1) * limit;
-
-  let timeNow = new Date(moment().format("YYYY-MM-DD HH:MM"));
-  let afterOneHour = new Date(
-    moment().add(1, "hours").format("YYYY-MM-DD HH:MM")
-  );
-  let rooms = await Rooms.find({
-    $and: [
-      { dateAndTime: { $gte: afterOneHour } },
-      {
-        $or: [
-          {
-            $and: [{ isPrivate: true }, { inviteOrScheduledUser: { $ne: id } }],
-          },
-        ],
-      },
-    ],
-  });
-  let roomsdata = rooms.slice(skip, skip + limit);
-  let totalPages = Math.ceil(rooms.length / limit);
-  rooms && res.status(200).json({ totalPages, data: roomsdata });
   try {
+    let { id, page, limit } = req.query;
+    page = page ? Number(page) : 1;
+    limit = limit ? Number(limit) : 20;
+    let skip = (page - 1) * limit;
+
+    let timeNow = new Date(moment().format("YYYY-MM-DD HH:MM"));
+    let afterOneHour = new Date(
+      moment().add(1, "hours").format("YYYY-MM-DD HH:MM")
+    );
+    let filter = {
+      $and: [
+        { dateAndTime: { $gte: afterOneHour } },
+        {
+          $or: [
+            {
+              isPrivate: false,
+              inviteOrScheduledUser: { $ne: Types.ObjectId(id) },
+            },
+            {
+              isPrivate: true,
+              inviteOrScheduledUser: { $ne: Types.ObjectId(id) },
+            },
+          ],
+        },
+      ],
+    };
+    let rooms = await Rooms.aggregate([
+      { $match: filter },
+      {
+        $lookup: {
+          from: "users",
+          localField: "hostId",
+          foreignField: "_id",
+          as: "hostData",
+        },
+      },
+      { $unwind: "$hostData" },
+      {
+        $project: {
+          "hostData._id": 1,
+          "hostData.userName": 1,
+          "hostData.name": 1,
+          "hostData.photoUrl": 1,
+          specialGuest: 1,
+          inviteOrScheduledUser: 1,
+          name: 1,
+          dateAndTime: 1,
+          isPrivate: 1,
+          createdAt: 1,
+          updatedAt: 1,
+        },
+      },
+    ]);
+
+    let roomsdata = rooms.slice(skip, skip + limit);
+    let totalPages = Math.ceil(rooms.length / limit);
+    rooms && res.status(200).json({ totalPages, data: roomsdata });
   } catch (error) {
     next(error);
   }
 };
-
 module.exports = {
   addRooms,
   getRoomsByUserId,
