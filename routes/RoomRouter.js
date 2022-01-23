9826ae19edf728b765bb07d0ef6cdc3090dd7b68
Jenish <kavathiyajenish008@gmail.com>
2022-01-23 23:35:05 +0530

    live rooms,scheduled,upcomming api

--- routes/RoomRouter.js
+++ routes/RoomRouter.js
@@ -5,11 +5,17 @@ const {
   getRoomByHost,
   deleteRoom,
   UpdateRoom,
+  getLiveRooms,
+  getScheduledRoom,
+  getUpcommingRoom,
 } = require("../controllers/rooms/main");
 
 const RoomRouter = express.Router();
 
 RoomRouter.post("/", addRooms);
+RoomRouter.get("/liveRooms", getLiveRooms);
+RoomRouter.get("/ScheduledRoom", getScheduledRoom);
+RoomRouter.get("/upcommingRoom", getUpcommingRoom);
 RoomRouter.get("/:userId", getRoomsByUserId);
 RoomRouter.get("/host/:hostId", getRoomByHost);
 RoomRouter.delete("/", deleteRoom);
